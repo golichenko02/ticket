9195e6bfdadb84053d212b94feb51819714ca6d2
Volodymyr Holichenko <vladimirgolicenko154@gmail.com>
2021-05-15 14:16:29 +0300

    created procedure find_all

--- src/main/resources/dump.sql
+++ src/main/resources/dump.sql
@@ -1,16 +1,13 @@
-CREATE TABLE journey
-(
-    id           SERIAL PRIMARY KEY,
-    station_from VARCHAR(100) NOT NULL,
-    station_to   VARCHAR(100) NOT NULL,
-    departure    DATE         NOT NULL,
-    arrival      DATE         NOT NULL,
-    route        VARCHAR(200) NOT NULL
-);
-
-INSERT INTO journey (station_from, station_to, departure, arrival, route)
-VALUES ('Odessa', 'Lviv', '2021 - 03 - 25', '2021 - 03 - 26', 'Odessa -> Lviv'),
-       ('Odessa', 'Kyiv', '2021-03-27', '2021-03-28', 'Odessa -> Kyiv'),
-       ('Kyiv', 'Lviv', '2021-03-23', '2021-03-24', 'Kyiv -> Lviv'),
-       ('Kyiv', 'Lviv', '2021-03-25', '2021-03-26', 'Kyiv -> Lviv'),
-       ('Kyiv', 'Lviv', '2021-04-14', '2021-04-15', 'Kyiv -> Lviv');
+create or replace function find_all(p_db_name varchar(20)) returns refcursor as
+$$
+declare
+db_cursor refcursor;
+begin
+open db_cursor for execute format('select  * from %I', p_db_name);
+return db_cursor;
+EXCEPTION
+    WHEN undefined_table THEN
+        RAISE NOTICE 'Illegal operation: %', SQLERRM;
+        RAISE;
+end;
+$$ language plpgsql;
