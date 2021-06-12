116b3edc33cbf067e226bfca85c736fadb97fa95
Volodymyr Holichenko <vladimirgolicenko154@gmail.com>
2021-06-12 21:14:23 +0300

    HW7 - added specification enum and value fields

--- src/main/java/org/hillel/service/query_info/PaginationInfo.java
+++ src/main/java/org/hillel/service/query_info/PaginationInfo.java
@@ -1,15 +1,23 @@
 package org.hillel.service.query_info;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hillel.persistence.jpa.repository.specification.ISpecification;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.util.StringUtils;
 
 @Getter
+@AllArgsConstructor
 public class PaginationInfo {
 
     private int pageNumber;
     private int pageSize;
     private String orderField;
     private boolean isAsc;
+    private ISpecification specificationFilter;
+    private String value;
+
 
     public PaginationInfo(int pageNumber, int pageSize, String orderField, boolean isAsc) {
         this.pageNumber = pageNumber;
@@ -28,4 +36,15 @@ public class PaginationInfo {
         }
         return "";
     }
+
+    public PageRequest createPageRequest() {
+        if (orderField == null || orderField.isEmpty()) {
+            return PageRequest.of(getPageNumber(), getPageSize());
+        }
+        return PageRequest.of(getPageNumber(),
+                getPageSize(),
+                Sort.by(isAsc() ? Sort.Direction.ASC : Sort.Direction.DESC, orderField));
+
+    }
+
 }
