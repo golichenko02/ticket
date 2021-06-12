aee1468d4ab86449bd48beea86f1a96494574350
Volodymyr Holichenko <vladimirgolicenko154@gmail.com>
2021-06-12 21:18:01 +0300

    HW7 - test new homework

--- src/main/java/org/hillel/Starter.java
+++ src/main/java/org/hillel/Starter.java
@@ -3,9 +3,12 @@ package org.hillel;
 import org.hillel.config.RootConfig;
 import org.hillel.persistence.entity.*;
 import org.hillel.persistence.entity.enums.DirectionType;
+import org.hillel.persistence.jpa.repository.specification.JourneySpecification;
+import org.hillel.persistence.jpa.repository.specification.StopSpecification;
+import org.hillel.persistence.jpa.repository.specification.VehicleSeatSpecification;
+import org.hillel.persistence.jpa.repository.specification.VehicleSpecification;
 import org.hillel.service.TicketClient;
 import org.hillel.service.query_info.PaginationInfo;
-import org.hillel.service.query_info.QueryType;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -20,12 +23,10 @@ public class Starter {
         final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
         TicketClient ticketClient = applicationContext.getBean(TicketClient.class);
 
-        VehicleEntity train = buildVehicle(buildCommonInfo("Черноморец", "Скоростной фирменный поезд"),
-                100, buildSeats(true, 1, 2, 3, 4, 5, 6, 50, 52, 60, 71));
+        VehicleEntity train = buildVehicle("Черноморец", 100, buildSeats(true, 1, 2, 3, 4, 5, 6, 50, 52, 60, 71));
 
 
-        VehicleEntity plain = buildVehicle(buildCommonInfo("Turkish Airlines", "перевозчик Турции"),
-                80, buildSeats(false, 10,20,25,28,30,70,76));
+        VehicleEntity plain = buildVehicle("Turkish Airlines", 80, buildSeats(false, 10, 20, 25, 28, 30, 70, 76));
 
         train = ticketClient.createOrUpdateVehicle(train);
 
@@ -53,24 +54,24 @@ public class Starter {
         ticketClient.createOrUpdateVehicle(train);
 
 
-        System.out.println("HQL");
-        System.out.println(ticketClient.findAllStops(QueryType.HQL, new PaginationInfo(3, 5, "active", true)));
-        System.out.println(ticketClient.findAllJourneys(QueryType.HQL, new PaginationInfo(5, 10, "id", false)));
-        System.out.println(ticketClient.findAllVehicles(QueryType.HQL, new PaginationInfo(8, 5, "name", true)));
-        System.out.println("CRITERIA");
-        System.out.println(ticketClient.findAllStops(QueryType.CRITERIA, new PaginationInfo(3, 5, "commonInfo", false)));
-        System.out.println(ticketClient.findAllJourneys(QueryType.CRITERIA, new PaginationInfo(3, 5, "stationFrom", true)));
-        System.out.println(ticketClient.findAllVehicles(QueryType.CRITERIA, new PaginationInfo(3, 5, "id", false)));
-        System.out.println("NATIVE");
-        System.out.println(ticketClient.findAllStops(QueryType.NATIVE, new PaginationInfo(4, 3, "name", false)));
-        System.out.println(ticketClient.findAllJourneys(QueryType.NATIVE, new PaginationInfo(5, 5, "active", true)));
-        System.out.println(ticketClient.findAllVehicles(QueryType.NATIVE, new PaginationInfo(5, 10, "active", true)));
+        System.out.println("FIND ALL WITH SPECIFICATION");
+        System.out.println(ticketClient.findAllVehiclesWithSpecification(new PaginationInfo(2, 3, "name", true, VehicleSpecification.BY_ONLY_ACTIVE, null)));
+        System.out.println(ticketClient.findAllStopsWithSpecification(new PaginationInfo(1, 2, "active", true, StopSpecification.BY_NAME, "Киев-Пасс.")));
+        System.out.println(ticketClient.findAllSeatsWithSpecification(new PaginationInfo(1, 3, "id", false, VehicleSeatSpecification.BY_ONLY_BOOKED, null)));
+        System.out.println(ticketClient.findAllJourneysWithSpecification(new PaginationInfo(1, 2, "id", true, JourneySpecification.BY_STATION_FROM, "Одесса")));
+
+        System.out.println("FIND ALL");
+        System.out.println(ticketClient.findAllVehicles(new PaginationInfo(0, 2, "name", true)));
+        System.out.println(ticketClient.findAllStops(new PaginationInfo(1, 2, "id", true)));
+        System.out.println(ticketClient.findAllSeats(new PaginationInfo(2, 2, "active", false)));
+        System.out.println(ticketClient.findAllJourneys(new PaginationInfo(3, 2, "id", false)));
 
         System.out.println("Транспортные средства с наименьшим количеством свободных мест");
         System.out.println(ticketClient.findVehicleWithMinFreeSeats());
 
         System.out.println("Транспортные средства с наибольшим количеством свободных мест");
         System.out.println(ticketClient.findVehicleWithMaxFreeSeats());
+
 
     }
 
@@ -102,9 +103,9 @@ public class Starter {
         return stopEntity;
     }
 
-    private static VehicleEntity buildVehicle(final CommonInfo commonInfo, final int maxSeats, final List<VehicleSeatEntity> seats) {
+    private static VehicleEntity buildVehicle(final String name, final int maxSeats, final List<VehicleSeatEntity> seats) {
         final VehicleEntity vehicleEntity = new VehicleEntity();
-        vehicleEntity.setCommonInfo(commonInfo);
+        vehicleEntity.setName(name);
         vehicleEntity.setMaxSeats(maxSeats);
         vehicleEntity.setActive(true);
         for (VehicleSeatEntity seat : seats)
